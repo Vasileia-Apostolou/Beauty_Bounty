e84bc518acaa301ff0e0262416f80a500ac27b94
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2021-01-27 12:14:17 +0000

    Add app URL to ALLOWED_HOSTS

--- beauty_bounty/settings.py
+++ beauty_bounty/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vas14899-beauty-bounty.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['beauty-bounty.herokuapp.com', 'localhost']
 
 
 # Application definition
